2c0ffaab7b4fecc727e04da4c5b7b79a42f5abce
Muhammad Ahmed Jamil <ahmed.jamil@koderlabs.com>
2022-10-09 23:15:24 +0500

    made components: PasswordField, Signup Form, and Top Bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
+import TopBar from "../components/TopBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <TopBar />
+      <Box maxWidth="container.xl" margin="auto">
+        <Component {...pageProps} />
+      </Box>
     </ChakraProvider>
   );
 }
